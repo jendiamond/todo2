b7826340350722635d0106e67b3506a5b0bedda2
jendiamond <thejendiamond@gmail.com>
2016-07-17 14:28:12 -0700

    Initial

--- src/todo.ts
+++ src/todo.ts
@@ -1,12 +1,29 @@
 import {Component} from 'angular2/core';
 
+class Todo {
+  task:string;
+  done:boolean;
+  constructor(_task:string){
+    this.task = task;
+    this.task = false;
+  }
+}
+
 @Component({
   selector:'todo',
   templateUrl: './templates/todo.html'
 })
 
 export class TodoComponent{
-  constructor(){
+  public task:string;
+  public tasks:Array<Todo> = [];
+  // or public tasks:string[];
 
+  constructor(){
+  }
+
+  onSubmit() {
+    console.log(JSON.stringify(this));
+    this.tasks.push(new Todo(this.task))
   }
 }
