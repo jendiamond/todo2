13ac9d355651d36e8a7cee2fda6030af1e9c231c
Rockncoder <rockncoder@gmail.com>
2016-07-17 14:27:37 -0700

    Accepts the task.

--- src/todo.ts
+++ src/todo.ts
@@ -1,12 +1,30 @@
 import {Component} from 'angular2/core';
 
+class Todo {
+  task:string;
+  done:boolean;
+  constructor(_task:string){
+    this.task = _task;
+    this.done = false;
+  }
+}
+
 @Component({
   selector:'todo',
   templateUrl: './templates/todo.html'
 })
 
 export class TodoComponent{
+  public task:string;
+  public tasks:Array<Todo> = [];
+  // public tasks:string[];
+
   constructor(){
+  }
+
+  onSubmit() {
+    console.log(JSON.stringify(this));
+    this.tasks.push(new Todo(this.task));
 
   }
 }
